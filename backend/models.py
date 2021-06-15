019bb08eb7184110a97a9181716143f93556fcb9
LuciusLux <77528299+LuciusLux@users.noreply.github.com>
2021-06-15 21:42:14 +0200

    Models erstellt

---
+++ backend/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create model Contact (type(Text20, selectlist), name(Text256), email(Text256, email valid), salutation(Text256)).
+class Contact(models.Model):
+    type = models.CharField(max_length=20)
+    name = models.CharField(max_length=256)
+    email = models.models.EmailField(_(""), max_length=256)
+    salutation = models.CharField(max_length=256)
+
+# Create model Country (key(Text20), value(Text256)).
+class Country(models.Model):
+    key = models.CharField(max_length=20, primary_key=True)
+    value = models.CharField(max_length=256)
+
+# Create model Address (street(Text256), zip(Text10), city(Text256)).
+class Address(models.Model):
+    street = models.CharField(max_length=256)
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=256)
+
+# Create model Invoice (title(Text256), body(longtext), date(datum), due(datum), condition(Text256)).
+class Invoice(models.Model):
+    title = models.CharField(max_length=256)
+    body = models.TextField(blank=True)
+    date = models.models.DateField(_(""), auto_now=False, auto_now_add=False)
+    due = models.models.DateField(_(""), auto_now=False, auto_now_add=False)
+    condition = models.CharField(max_length=256)
+
+# Create model InvoicePosition (title(Text256), body(longtext), quantity(number dezi), amount(number dezi)).
+class InvoicePosition(models.Model):
+    title = models.CharField(max_length=256)
+    body = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(_(""))
+    amount = models.DecimalField(_(""), max_digits=8, decimal_places=2)
