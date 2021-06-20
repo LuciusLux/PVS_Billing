ab121dd02a19691485cf5c9abb1a5b6b9bbc3816
LuciusLux <77528299+LuciusLux@users.noreply.github.com>
2021-06-20 19:43:54 +0200

    admin inline search and co

--- backend/admin.py
+++ backend/admin.py
@@ -3,15 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Contact, Country, Address, Invoice, InvoicePosition
 
+# Inline for the models here.
+class ContactInline(admin.StackedInline):
+    model = Address
+    extra = 0
+
+class InvoiceInline(admin.StackedInline):
+    model = InvoicePosition
+    extra = 0
+# Register Contact (Inline, filter, search, list).
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ContactInline]
+    list_filter = ['type']
+    search_fields = ['name']
 
-
+# Register Country (search).
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['value', 'key']
 
+# Register Invoice (Inline, search, list).
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
-    pass
+    inlines = [InvoiceInline]
+    search_fields = ['name', 'title']
